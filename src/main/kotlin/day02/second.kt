47b8fec88e6e4792d4907dd87e596a6ec086491f
T45K <tasktas9@gmail.com>
2024-12-02 22:52:02 +0900

    day02

--- src/main/kotlin/day02/second.kt
+++ src/main/kotlin/day02/second.kt
@@ -6,9 +6,17 @@ import resourcePath
 fun main() {
     val lines = resourcePath.resolve("day02.txt").readLines()
     val answer = lines.map { line -> line.split(" ").map { it.toInt() } }
-        .count { it.isIncreasing() || it.isDecreasing() }
+        .count { list ->
+            val candidates = listUpCandidates(list)
+            candidates.any { it.isIncreasing() } ||
+                candidates.any { it.isDecreasing() }
+        }
     println(answer)
 }
+
+private fun listUpCandidates(list: List<Int>): List<List<Int>> = list.mapIndexed { index, _ -> list.removeAt(index) } + listOf(list)
+
+private fun List<Int>.removeAt(index: Int): List<Int> = filterIndexed { idx, _ -> index != idx }
 
 private fun List<Int>.isIncreasing(): Boolean = zipWithNext().all { it.first < it.second && (it.second - it.first <= 3) }
 private fun List<Int>.isDecreasing(): Boolean = zipWithNext().all { it.first > it.second && (it.first - it.second <= 3) }
