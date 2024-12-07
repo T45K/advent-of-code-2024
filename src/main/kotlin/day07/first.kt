25129e07019841af1f6211b5392f17e3ffe4ed57
T45K <tasktas9@gmail.com>
2024-12-07 15:23:14 +0900

    day07 first

---
+++ src/main/kotlin/day07/first.kt
@@ -0,0 +1,27 @@
+package day07
+
+import kotlin.io.path.readLines
+import resourcePath
+
+fun main() {
+    val lines = resourcePath.resolve("day07.txt").readLines()
+    val answer = lines.sumOf { line ->
+        val split = line.split(": ")
+        val testValue = split[0].toLong()
+        val numbers = split[1].split(" ").map { it.toLong() }
+        if (calculate(numbers, 1, numbers[0], testValue)) {
+            testValue
+        } else {
+            0
+        }
+    }
+    println(answer)
+}
+
+private fun calculate(list: List<Long>, index: Int, total: Long, testValue: Long): Boolean =
+    if (index == list.size) {
+        total == testValue
+    } else {
+        calculate(list, index + 1, total + list[index], testValue) ||
+            calculate(list, index + 1, total * list[index], testValue)
+    }
